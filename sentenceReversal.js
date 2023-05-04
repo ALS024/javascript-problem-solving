0ad2301e2e6cae50cd6f46efb294f43757d33157
Adriana Sawyer <14203266+ALS024@users.noreply.github.com>
2023-05-04 13:34:22 -0400

    Create sentenceReversal.js

---
+++ sentenceReversal.js
@@ -0,0 +1,16 @@
+
+let strInput = "This is a coding test, I like coding because it is fun"
+let strOutput = strInput.reverse();
+
+// for (let char of strInput) {
+//    strOutput = char + strOutput;
+// }
+
+function removeDuplicatesToRight<T>(array: T[]) {
+   return array.filter((value, index) => array.indexOf(value) === index);
+}
+
+var arrOutput = new Array[strOutput];
+removeDuplicatesToRight(arrOutput);
+
+
